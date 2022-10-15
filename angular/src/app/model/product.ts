fced386e85692fc7e82951737665cdf59db5a881
SWATHI <swathi@specrio.co>
2022-10-15 20:11:23 +0530

    nn

---
+++ angular/src/app/model/product.ts
@@ -0,0 +1,13 @@
+export class Product{
+    id:number;
+    name:string;
+    desc:string;
+    customername:string;
+    mobilenum:number;
+    category:string;
+    actualPrice:number;
+    discount:number;
+    price:number;
+    avail:string;
+    imagepath:string;
+}
